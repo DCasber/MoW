ee7b5eb148ca6d5cea1f900e1020bfbed94ba0b9
David Castillo Berná <davidcastillo200.dc@gmail.com>
2021-12-27 12:33:35 +0100

    features: creación mostrarActividad

---
+++ app/app/src/main/java/com/example/appmow/MostrarTarea.java
@@ -0,0 +1,14 @@
+package com.example.appmow;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MostrarTarea extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mostrar_tarea);
+    }
+}
