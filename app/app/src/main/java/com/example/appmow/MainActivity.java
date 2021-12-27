14115903e2caaaff4afc467433aef9362b1aa8f3
Alex <alexrametespinosa@uma.es>
2021-12-27 13:28:00 +0100

    get todas las tareas

--- app/app/src/main/java/com/example/appmow/MainActivity.java
+++ app/app/src/main/java/com/example/appmow/MainActivity.java
@@ -3,7 +3,11 @@ package com.example.appmow;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
+import android.util.Pair;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -12,12 +16,17 @@ import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.ListIterator;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
     private ListView lista;
-    private String[] tareas;
+    private List<Pair<String, String>> tareas;
 
 
     @Override
@@ -30,6 +39,61 @@ public class MainActivity extends AppCompatActivity {
 
         // CONSEGUIR TAREAS DE LA BD
         // tareas = getResources().getStringArray(R.array.array_technology);
+
+        TareaHelper th = new TareaHelper(getApplicationContext(), "database_name.db");
+        SQLiteDatabase db = th.getReadableDatabase();
+
+        Map<Integer, List<Pair<String, String>>> listBD = new HashMap<>();
+
+        String [] datos = {
+                Tarea.DictEntry._ID,
+                Tarea.DictEntry.COLUMN_NAME_KEY_ASUNTO,
+                Tarea.DictEntry.COLUMN_NAME_VAL_ASUNTO,
+                Tarea.DictEntry.COLUMN_NAME_KEY_UB_DESTINO,
+                Tarea.DictEntry.COLUMN_NAME_VAL_UB_DESTINO,
+                Tarea.DictEntry.COLUMN_NAME_KEY_ALARMA,
+                Tarea.DictEntry.COLUMN_NAME_VAL_ALARMA
+        } ;
+
+        String sortOrder = Tarea.DictEntry.COLUMN_NAME_KEY_FECHA + " ASC";
+        Cursor cursor = db.query(Tarea.DictEntry.TABLE_NAME, datos, null, null, null, null, sortOrder);
+        try {
+            while (cursor.moveToNext()) {
+                List<Pair<String, String>> aux = new ArrayList<>();
+                Integer id = cursor.getInt(cursor.getColumnIndexOrThrow(Tarea.DictEntry._ID));
+
+                String claveAsunto = cursor.getString(cursor.getColumnIndexOrThrow(Tarea.DictEntry.COLUMN_NAME_KEY_ASUNTO));
+                String valorAsunto = cursor.getString(cursor.getColumnIndexOrThrow(Tarea.DictEntry.COLUMN_NAME_VAL_ASUNTO));
+                Pair<String, String> asunto = new Pair<>(claveAsunto, valorAsunto);
+                aux.add(asunto);
+
+                String claveUbicacion = cursor.getString(cursor.getColumnIndexOrThrow(Tarea.DictEntry.COLUMN_NAME_KEY_UB_DESTINO));
+                String valorUbicacion = cursor.getString(cursor.getColumnIndexOrThrow(Tarea.DictEntry.COLUMN_NAME_VAL_UB_DESTINO));
+                Pair<String, String> ubicacion = new Pair<>(claveUbicacion, valorUbicacion);
+                aux.add(ubicacion);
+
+                String claveAlarma = cursor.getString(cursor.getColumnIndexOrThrow(Tarea.DictEntry.COLUMN_NAME_KEY_ALARMA));
+                String valorAlarma = cursor.getString(cursor.getColumnIndexOrThrow(Tarea.DictEntry.COLUMN_NAME_VAL_ALARMA));
+                Pair<String, String> alarma = new Pair<>(claveAlarma, valorAlarma);
+                aux.add(alarma);
+
+                listBD.put(id, aux);
+            }
+        } finally {
+            cursor.close();
+        }
+
+        tareas = new ArrayList<>();
+
+        for (List<Pair<String, String>> t : listBD.values()){
+            //t = key_asunto, val_asunto, key_ubicadst, val_ubicadst, key_alarma, val_alarma
+            for (ListIterator<Pair<String, String>> it = t.listIterator(); it.hasNext(); ) {
+                Pair<String, String> v = it.next();
+                tareas.add(v);
+
+            }
+        }
+
 
         final ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
                 android.R.layout.simple_list_item_1, android.R.id.text1, tareas);
