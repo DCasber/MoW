d80ede54a652c842655f0655740716f26469836d
David Castillo Berná <davidcastillo200.dc@gmail.com>
2021-12-28 16:26:35 +0100

    feature: MostrarTarea 1.0

--- app/app/src/main/java/com/example/appmow/MostrarTarea.java
+++ app/app/src/main/java/com/example/appmow/MostrarTarea.java
@@ -1,5 +1,6 @@
 package com.example.appmow;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.database.Cursor;
@@ -8,21 +9,23 @@ import android.os.Bundle;
 import android.util.Pair;
 import android.widget.TextView;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.MapView;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MostrarTarea extends AppCompatActivity {
+public class MostrarTarea extends AppCompatActivity{
 
     final TextView tAsunto = (TextView) findViewById(R.id.nombreTarea);
     final TextView tFecha = (TextView) findViewById(R.id.fechaTarea);
     final TextView tAlarma = (TextView) findViewById(R.id.alarmaTarea);
     final TextView tTransporte = (TextView) findViewById(R.id.transporteTarea);
-    final MapView mOrigen = (MapView) findViewById(R.id.ubicacionPersona);
-    final MapView mDestino = (MapView) findViewById(R.id.ubicacionTarea);
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,7 +55,7 @@ public class MostrarTarea extends AppCompatActivity {
                 Tarea.DictEntry.COLUMN_NAME_VAL_FECHA,
         };
 
-        Cursor cursor = db.query(Tarea.DictEntry.TABLE_NAME, datos, "_ID = tarea", null, null, null, null);
+        Cursor cursor = db.query(Tarea.DictEntry.TABLE_NAME, datos, "_ID = " + tarea, null, null, null, null);
         try {
 
                 asunto = cursor.getString(cursor.getColumnIndexOrThrow(Tarea.DictEntry.COLUMN_NAME_VAL_ASUNTO));
@@ -66,16 +69,47 @@ public class MostrarTarea extends AppCompatActivity {
             cursor.close();
         }
 
-
         tAsunto.setText(asunto);
         tAlarma.setText(alarma);
         tTransporte.setText(transporte);
         tFecha.setText(fecha);
 
+        double latOrigen, latDestino, longOrigen, longDestino;
+
+        String [] ubOrigen = ubicacionOrigen.split(",");
+        String [] ubDestino = ubicacionDestino.split(",");
+
+        latOrigen = Integer.parseInt(ubOrigen[0]);
+        latDestino = Integer.parseInt(ubDestino[0]);
+        longOrigen = Integer.parseInt(ubOrigen[1]);
+        longDestino = Integer.parseInt(ubDestino[1]);
+
+        com.google.android.gms.maps.MapFragment mapOrigen = (MapFragment) getFragmentManager().findFragmentById(R.id.ubicacionPersona);
+        mapOrigen.getMapAsync(onMapReadyCallback1(latOrigen, longOrigen));
+        com.google.android.gms.maps.MapFragment mapDestino = (MapFragment) getFragmentManager().findFragmentById(R.id.ubicacionTarea);
+        mapDestino.getMapAsync(onMapReadyCallback2(latDestino, longDestino));
 
 
 
+    }
 
+    public OnMapReadyCallback onMapReadyCallback1(double lat, double lon){
+        return googleMap -> {
+            GoogleMap mMap = googleMap;
+            LatLng latlng = new LatLng(lat, lon);
+            mMap.addMarker(new MarkerOptions().position(latlng).title("Origen"));
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(latlng));
+        };
+    }
+
+    public OnMapReadyCallback onMapReadyCallback2(double lat, double lon){
+        return googleMap -> {
+            GoogleMap mMap = googleMap;
+            LatLng latlng = new LatLng(lat, lon);
+            mMap.addMarker(new MarkerOptions().position(latlng).title("Destino"));
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(latlng));
+
+        };
     }
 
 }
